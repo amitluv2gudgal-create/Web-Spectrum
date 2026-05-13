6edd9a9e9245f86868351b5f375a681396ae333b
Omprakash Rai <amitluv2gudgal@gmail.com>
2026-05-13 20:35:07 +0530

    Render deployment fix

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get("/api/leads", (req, res) => {
     );
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server Running on Port ${PORT}`);
