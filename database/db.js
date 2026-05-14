6851f412ba61f4cbec0e5d8260524378bcbd59f5
Omprakash Rai <amitluv2gudgal@gmail.com>
2026-05-14 11:45:28 +0530

    Added JWT admin authentication

--- database/db.js
+++ database/db.js
@@ -16,4 +16,37 @@ db.prepare(`
     )
 `).run();
 
+/* ADMIN TABLE */
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS admins (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+`).run();
+
+const bcrypt = require("bcryptjs");
+
+const adminExists = db
+.prepare("SELECT * FROM admins WHERE username = ?")
+.get("admin");
+
+if(!adminExists){
+
+    const hashedPassword = bcrypt.hashSync("admin123", 10);
+
+    db.prepare(`
+        INSERT INTO admins (
+            username,
+            password
+        )
+        VALUES (?, ?)
+    `).run(
+        "admin",
+        hashedPassword
+    );
+
+    console.log("✅ Default admin created");
+}
+
 module.exports = db;
