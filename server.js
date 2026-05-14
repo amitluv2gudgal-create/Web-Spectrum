a7d4832613db2290f8ccc01973f3d0735d9ae57f
Omprakash Rai <amitluv2gudgal@gmail.com>
2026-05-14 11:46:38 +0530

    Added JWT admin authentication

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const db = require("./database/db");
+
+
+const SECRET = "BLACKCAT_SECRET_KEY";
+
 
 const app = express();
 
@@ -9,6 +14,90 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static("public"));
+
+/* ADMIN LOGIN */
+app.post("/api/login", (req, res) => {
+
+    const {
+        username,
+        password
+    } = req.body;
+
+    const admin = db
+    .prepare(`
+        SELECT *
+        FROM admins
+        WHERE username = ?
+    `)
+    .get(username);
+
+    if(!admin){
+
+        return res.status(401).json({
+            error: "Invalid username"
+        });
+    }
+
+    const validPassword = bcrypt.compareSync(
+        password,
+        admin.password
+    );
+
+    if(!validPassword){
+
+        return res.status(401).json({
+            error: "Invalid password"
+        });
+    }
+
+    const token = jwt.sign(
+        {
+            id: admin.id,
+            username: admin.username
+        },
+        SECRET,
+        {
+            expiresIn: "1d"
+        }
+    );
+
+    res.json({
+        success: true,
+        token
+    });
+});
+
+function authenticate(req, res, next){
+
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+
+        return res.status(401).json({
+            error: "Access denied"
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+
+        const verified = jwt.verify(
+            token,
+            SECRET
+        );
+
+        req.user = verified;
+
+        next();
+
+    } catch(err){
+
+        res.status(401).json({
+            error: "Invalid token"
+        });
+    }
+}
 
 /* SAVE LEAD */
 app.post("/api/leads", (req, res) => {
@@ -55,7 +144,7 @@ app.post("/api/leads", (req, res) => {
 });
 
 /* GET LEADS */
-app.get("/api/leads", (req, res) => {
+app.get("/api/leads", authenticate, (req, res) => {
 
     try {
 
